c3649d3002211f2fbde55d9ad6117745f51fb1ec
RishBez <rishabhbezbaruah@gmail.com>
2025-07-12 17:15:38 +0530

    Preliminary Setup

---
+++ gwr_nfhs_assam.R
@@ -0,0 +1,11 @@
+# Clearing Environment
+rm(list = ls())
+# Working Directory
+getwd()
+# Setting Working Directory
+setwd("D:\\GWR_NFHS_Assam")
+# Verifying Working Directory
+getwd()
+
+# Packages
+library(tidyverse)
